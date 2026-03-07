c26a8fddbf8ca7ae8347b048f22bba3924154536
ZANNOU Jesugnon Joel <Jesugnonjoel.zannou@edu.univ-paris13.fr>
2026-03-07 21:05:53 +0100

    suppression du conteneur demo nginx. il sera déployé directement via dockercompose dans le repo applicatif MERN

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "ubuntu" {
+data "aws_ami" "ubuntu" { # Fetch the latest Ubuntu 22.04 LTS AMI
   most_recent = true
   owners      = ["099720109477"] # Canonical
 
@@ -8,40 +8,39 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web" { # EC2 instance
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    set -eux
+user_data = <<-EOF
+  #!/bin/bash
+  set -eux
 
-    apt-get update -y
-    apt-get install -y ca-certificates curl gnupg
+  apt-get update -y
+  apt-get install -y ca-certificates curl gnupg git
 
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    chmod a+r /etc/apt/keyrings/docker.gpg
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  chmod a+r /etc/apt/keyrings/docker.gpg
 
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
-      > /etc/apt/sources.list.d/docker.list
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+    > /etc/apt/sources.list.d/docker.list
 
-    apt-get update -y
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  apt-get update -y
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-    systemctl enable docker
-    systemctl start docker
+  systemctl enable docker
+  systemctl start docker
 
-    docker run -d --restart unless-stopped --name nginx -p 80:80 nginx:stable
+  usermod -aG docker ubuntu
 
-    docker run -d --restart unless-stopped --name node-exporter \
-      -p 9100:9100 \
-      prom/node-exporter:latest
-  EOF
-
+  docker run -d --restart unless-stopped --name node-exporter \
+    -p 9100:9100 \
+    prom/node-exporter:latest
+EOF
   tags = {
     Name = "devops-fil-rouge-ec2"
   }
