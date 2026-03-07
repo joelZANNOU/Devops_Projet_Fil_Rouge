d66f20a1b887c1569d7713a722c9d2c47a00f6e1
ZANNOU Jesugnon Joel <Jesugnonjoel.zannou@edu.univ-paris13.fr>
2026-03-07 22:01:21 +0100

    ajout de keypair pour acces ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "web" { # EC2 instance
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-
+  key_name               = "keypair"
 user_data = <<-EOF
   #!/bin/bash
   set -eux
